e963c21f31bc906c99b57daede7b089bf754907b
Chris <chrisclementsresearch@gmail.com>
2017-12-12 15:28:14 +1100

    Various improvements
    Code changed to now run simulation. including growth of individuals, etc

--- tacticalCoral/Generic coral data.R
+++ tacticalCoral/Generic coral data.R
@@ -5,6 +5,13 @@
 coral.types<-data.frame(expand.grid("Structure"=c("Domed", "Branching", "Plating"), "Stratergy"=c("Competitive", "Weedy","Stress tolerant", "Generalist")))
 coral.types$unique.ID<-paste(coral.types$Structure, coral.types$Stratergy, sep="+")  
 coral.types$probability.occurance<-c(0, 95.7, 39.1, 56.5, 39.1, 21.7, 100, 0, 1.7, 100, 35.7, 100)
+##not quite sure what these proabbilities refer to at the moment, so I have normalised by stratergy them and used them to draw the random structure:
+coral.types$norm.prob<-NA
+coral.types$norm.prob[which(coral.types$Stratergy=="Competitive")]<-coral.types$probability.occurance[which(coral.types$Stratergy=="Competitive")]/sum(coral.types$probability.occurance[which(coral.types$Stratergy=="Competitive")])
+coral.types$norm.prob[which(coral.types$Stratergy=="Weedy")]<-coral.types$probability.occurance[which(coral.types$Stratergy=="Weedy")]/sum(coral.types$probability.occurance[which(coral.types$Stratergy=="Weedy")])
+coral.types$norm.prob[which(coral.types$Stratergy=="Stress tolerant")]<-coral.types$probability.occurance[which(coral.types$Stratergy=="Stress tolerant")]/sum(coral.types$probability.occurance[which(coral.types$Stratergy=="Stress tolerant")])
+coral.types$norm.prob[which(coral.types$Stratergy=="Generalist")]<-coral.types$probability.occurance[which(coral.types$Stratergy=="Generalist")]/sum(coral.types$probability.occurance[which(coral.types$Stratergy=="Generalist")])
+
 ##add in the generation time from the eco let paper, and its SD. We can use this later to draw random generation times for each individual generated
 coral.types$gen.time<-NA
 coral.types$gen.time.sd<-NA
@@ -43,3 +50,17 @@ coral.types$fecundity[which(coral.types$Stratergy=="Generalist")]<-18.46
 coral.types$fecundity.sd[which(coral.types$Stratergy=="Generalist")]<-9.26
 coral.types$fecundity[which(coral.types$Stratergy=="Stress tolerant")]<-372.45
 coral.types$fecundity.sd[which(coral.types$Stratergy=="Stress tolerant")]<-694.15
+##also add in growth rates, incase we want to make it growth dependant too...
+coral.types$growth<-NA
+coral.types$growth.sd<-NA
+coral.types$growth[which(coral.types$Stratergy=="Competitive")]<-49.83
+coral.types$growth.sd[which(coral.types$Stratergy=="Competitive")]<-41.28
+coral.types$growth[which(coral.types$Stratergy=="Weedy")]<-11.35
+coral.types$growth.sd[which(coral.types$Stratergy=="Weedy")]<-10.04
+coral.types$growth[which(coral.types$Stratergy=="Generalist")]<-19.18
+coral.types$growth.sd[which(coral.types$Stratergy=="Generalist")]<-11.58
+coral.types$growth[which(coral.types$Stratergy=="Stress tolerant")]<-7.98
+coral.types$growth.sd[which(coral.types$Stratergy=="Stress tolerant")]<-6.69
+##then make it into cm (as the sizes are)
+coral.types$growth<-coral.types$growth/10
+coral.types$growth.sd<-coral.types$growth.sd/10
